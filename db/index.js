de8ced320e08ba640ee1469cbae427446d7d03d1
Creigh Battle <creighbattle@gmail.com>
2021-02-11 04:06:37 -0500

    postgres-update-1

--- db/index.js
+++ db/index.js
@@ -10,15 +10,19 @@ const devConfig = {
   databse: process.env.PG_DATABASE,
   port: process.env.PG_PORT,
 };
-console.log(devConfig.port);
+console.log(devConfigd.port);
 
 const proConfig = {
   connectionString: process.env.DATABASE_URL, //heroku addons
 };
 
-const pool = new Pool(
-  process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
+const pool = new Pool({
+  // process.env.NODE_ENV === "production" ? proConfig : devConfig
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 // module.exports = {
 //   query: (text, params) => pool.query(text, params),
